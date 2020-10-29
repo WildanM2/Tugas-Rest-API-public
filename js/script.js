0aa980158920be76eade560e7efa1a16192644c2
Wildan Maulidi <wildancb08@gmail.com>
2020-10-29 10:17:39 +0700

    menambahkan pencetak gol

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@ const ApiKey = "0ca87e3576484c0c9eaeb2bfade1fb71";
 const baseUrl = "https://api.football-data.org/v2/";
 const leagueId = "2021";
 const baseEndPoin = `${baseUrl}competitions/${leagueId}`;
+const scorerEndPoin = `${baseUrl}competitions/${leagueId}/scorers`;
 const teamEndPoin = `${baseUrl}competitions/${leagueId}/teams`;
 const standingEndPoin = `${baseUrl}competitions/${leagueId}/standings`;
 const matchEndPoin = `${baseUrl}competitions/${leagueId}/matches`;
 
 const contents = document.querySelector("#content-list");
+const inf = document.querySelector("#detail-list");
 const title = document.querySelector(".card-title");
 const fetchHeader = {
     headers:{
@@ -16,7 +18,7 @@ const fetchHeader = {
 
 function getListTeams(){
     title.innerHTML = "Daftar Tim Liga Primer Inggris";
-    fetch(teamEndPoin, fetchHeader)
+    fetch(teamEndPoin, fetchHeader) 
     .then(response => response.json())
     .then(resJson=>{
         console.log(resJson.teams);
@@ -29,7 +31,7 @@ function getListTeams(){
                     <p>Berdiri: ${team.founded}<br>
                         Markas: ${team.venue}
                     </p>
-                    <a href="#!" class="secondary-content"><i class="material-icons">info</i></a>
+                    <a href="#modal1" class="secondary-content"><i class="material-icons">info</i></a>
                 </li>
             `
         });
@@ -38,6 +40,7 @@ function getListTeams(){
         console.error(err);
     })
 }
+
 
 function getListStandings(){
     title.innerHTML = "Klasemen Liga Primer Inggris";
@@ -128,6 +131,50 @@ function getListMatches(){
     })
 }
 
+function getListScorers(){
+    title.innerHTML = "Daftar Pecetak Gol Liga Primer Inggris";
+    fetch(scorerEndPoin, fetchHeader)
+    .then(response => response.json())
+    .then(resJson=>{
+        console.log(resJson.scorers[0]);
+        let scorers = "";
+        let i = 1;
+        resJson.scorers.forEach(scorer => {
+            scorers += `
+            <tr>
+                <td style="padding-left:20px;">${i}.</td>
+                <td>${scorer.player.name}</td>
+                <td>${scorer.player.nationality}</td>
+                <td>${scorer.team.name}</td>
+                <td>${scorer.player.position}</td>
+                <td>${scorer.numberOfGoals}</td>
+            </tr>
+            `;
+            i++;
+        });
+        contents.innerHTML = `
+        <div class="card">
+            <table class="stripped responsive-table">
+                <thead> 
+                   <th></th>
+                    <th>Nama Pemain</th>
+                    <th>Kebangsaan</th>
+                    <th>Tim</th>
+                    <th>Posisi</th>
+                    <th>Jumlah Gol</th>
+                </thead>
+                <tbody>
+                    ${scorers}
+                </tbody>
+            </table>
+        </div>
+        `
+    }).catch(err=>{
+        console.error(err);
+    })
+}
+
+
 function loadPage(page){
     switch(page){
         case "teams":
@@ -139,6 +186,9 @@ function loadPage(page){
         case "matches":
             getListMatches();
             break;
+        case "scorers":
+             getListScorers();
+             break;
     }
 }
 
@@ -158,3 +208,4 @@ document.addEventListener('DOMContentLoaded', function() {
     if (page === "" || page === "!") page = "teams";
     loadPage(page);
 });
+   
